2193cf162ff83e803067e94255e76ce5c3e918aa
joaogduarte <joaoduarte4567@gmail.com>
2026-05-11 22:02:25 -0300

    separa as mudanças de insturumentistas

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -50,7 +50,7 @@ CREATE TABLE Banda (
     ))
 );
 --ANTES
-/*CREATE TABLE Membro (
+CREATE TABLE Membro (
     cpf                 CHAR(11)        NOT NULL,
     nome                VARCHAR2(100)   NOT NULL,
     banda               NUMBER(10),
@@ -76,49 +76,6 @@ CREATE TABLE Banda (
     CONSTRAINT chk_membro_tipo_obrigatorio CHECK (
         eh_instrumentista = 'S' OR eh_vocalista = 'S'
     )
-);*/
-
-CREATE TABLE Membro (
-    cpf                 CHAR(11)        NOT NULL,
-    nome                VARCHAR2(100)   NOT NULL,
-    banda               NUMBER(10),
-
-    CONSTRAINT pk_membro PRIMARY KEY (cpf),
-    CONSTRAINT uq_membro_cpf_banda UNIQUE (cpf, banda),
-    
-    CONSTRAINT fk_membro_banda FOREIGN KEY (banda)
-        REFERENCES Banda(id_banda)
-        ON DELETE SET NULL,
-        
-    CONSTRAINT chk_membro_cpf CHECK (REGEXP_LIKE(cpf, '^[0-9]{11}$')),
-    CONSTRAINT chk_membro_instrumentista CHECK (eh_instrumentista IN ('S', 'N')),
-    CONSTRAINT chk_membro_vocalista CHECK (eh_vocalista IN ('S', 'N')),
-
-    -- Garante a Especialização TOTAL (obriga a ser pelo menos um dos dois)
-    CONSTRAINT chk_membro_tipo_obrigatorio CHECK (
-        eh_instrumentista = 'S' OR eh_vocalista = 'S'
-    )
-);
-
-
-CREATE TABLE Instrumentista (
-    cpf_membro          CHAR(11)        NOT NULL,
-    
-    CONSTRAINT pk_instrumentista PRIMARY KEY (cpf_membro),
-    
-    CONSTRAINT fk_inst_membro FOREIGN KEY (cpf_membro) 
-        REFERENCES Membro(cpf) 
-        ON DELETE CASCADE
-);
-
-CREATE TABLE Vocalista (
-    cpf_membro          CHAR(11)        NOT NULL,
-    
-    CONSTRAINT pk_vocalista PRIMARY KEY (cpf_membro),
-    
-    CONSTRAINT fk_voc_membro FOREIGN KEY (cpf_membro) 
-        REFERENCES Membro(cpf) 
-        ON DELETE CASCADE
 );
 
 ALTER TABLE Banda
@@ -138,8 +95,6 @@ CREATE TABLE Telefone_membro (
     CONSTRAINT chk_telefone CHECK (REGEXP_LIKE(telefone, '^[0-9]{10,11}$'))
 );
 
-/*
-ANTIGO
 
 CREATE TABLE Instrumento (
 
@@ -170,27 +125,8 @@ CREATE TABLE Instrumento (
     ))
 
 );
-*/
 
 
-CREATE TABLE Instrumento (
-    membro      CHAR(11)       NOT NULL,
-    cod_inv     NUMBER(10)     NOT NULL, 
-    tipo        VARCHAR2(60)   NOT NULL,
-    marca       VARCHAR2(60)   NOT NULL
-
-    CONSTRAINT pk_instrumento PRIMARY KEY (membro, cod_inv),
-    
-    -- A grande mudança: A Foreign Key agora aponta para a Subclasse!
-    CONSTRAINT fk_instrumento_membro FOREIGN KEY (membro)
-        REFERENCES Instrumentista(cpf_membro)
-        ON DELETE CASCADE,
-        
-    CONSTRAINT chk_instrumento_tipo CHECK (tipo IN (
-        'Guitarra', 'Baixo', 'Bateria', 'Teclado', 'Violao', 'Contrabaixo',
-        'Saxofone', 'Trompete', 'Flauta', 'Violino', 'Percussao', 'Outro'
-    ))
-);
 
 CREATE OR REPLACE TRIGGER trg_gera_cod_inv
 BEFORE INSERT ON Instrumento
